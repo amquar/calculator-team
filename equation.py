09e6b6e6a1c4a1b495b5c6afb8511733a4c3baec
hmmuu <hm.thidaratbm@gmail.com>
2024-09-04 09:09:53 +0700

    done division

--- equation.py
+++ equation.py
@@ -1,22 +1,22 @@
 # Equation function
 
-def calculate_equation(equation):
-    try:
-        result = eval(equation)
-        
-        print(f"The result of the equation '{equation}' is: {result}")
-    except ZeroDivisionError:
-        print("Error: Division by zero is not allowed.")
-    except Exception as e:
-        print(f"Error: Invalid equation. {str(e)}")
+class Equation:
+    def calculate_equation(equation):
+        try:
+              result = eval(equation)
+              print(result)
+              
+        except ZeroDivisionError:
+              print("Error: Division by zero is not allowed.")
+        except Exception as e:
+              print(f"Error: Invalid equation. {str(e)}")
 
-def calculator():
-    print("Welcome to the Equation Calculator")
-    print("You can use +, -, *, /, and parentheses in your equation.")
-    print("For example: (2 + 3) * 4 / 5")
-    
-    equation = input("Enter the equation: ")
-    
-    calculate_equation(equation)
+    def __init__(self):
+         self.current_input = ""
+         self.result = None
+         
+    def update_label(self):
+        display_text = self.current_input if self.current_input else str(self.result if self.result is not None else "")
+        self.label.config(text=display_text)
 
-calculator()
+Equation()
